583f40157534af2e3eae7f16f0e81b550b59b5de
SantiagoTaracena01 <staracenapuga@gmail.com>
2020-11-05 21:52:21 -0600

    Updates

---
+++ precache-manifest.de222dc31b946bc12bc5891560964748.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7681acd8c4f0e9ab91b3c01ba8933efb",
+    "url": "/ObjectOrientedProgramming.git/index.html"
+  },
+  {
+    "revision": "247be06890d985260643",
+    "url": "/ObjectOrientedProgramming.git/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "1be00cbceb022472ebde",
+    "url": "/ObjectOrientedProgramming.git/static/css/main.4625d05a.chunk.css"
+  },
+  {
+    "revision": "247be06890d985260643",
+    "url": "/ObjectOrientedProgramming.git/static/js/2.49b97ce0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ObjectOrientedProgramming.git/static/js/2.49b97ce0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1be00cbceb022472ebde",
+    "url": "/ObjectOrientedProgramming.git/static/js/main.baf356b5.chunk.js"
+  },
+  {
+    "revision": "ef51dfdbdda313219af1",
+    "url": "/ObjectOrientedProgramming.git/static/js/runtime-main.0cf2ff43.js"
+  },
+  {
+    "revision": "ef0ff96316622206d397c49587e027be",
+    "url": "/ObjectOrientedProgramming.git/static/media/algebra.ef0ff963.jpg"
+  },
+  {
+    "revision": "d60f2d9ce0daf20f1bc141fedbb69840",
+    "url": "/ObjectOrientedProgramming.git/static/media/aritmetic.d60f2d9c.jpg"
+  },
+  {
+    "revision": "c10ae8143827256897464f61be0325b3",
+    "url": "/ObjectOrientedProgramming.git/static/media/desc1.c10ae814.png"
+  },
+  {
+    "revision": "e5bb3e113241064fdf76d970f35ecf32",
+    "url": "/ObjectOrientedProgramming.git/static/media/desc2.e5bb3e11.png"
+  },
+  {
+    "revision": "1bb861bb51077892b8e990b6dc95396d",
+    "url": "/ObjectOrientedProgramming.git/static/media/desc3.1bb861bb.png"
+  },
+  {
+    "revision": "7c8353c17ead6d91b086cfb19793c49c",
+    "url": "/ObjectOrientedProgramming.git/static/media/eap.7c8353c1.png"
+  },
+  {
+    "revision": "42a4eaeec87372e12c459193794c9f10",
+    "url": "/ObjectOrientedProgramming.git/static/media/el_tio.42a4eaee.jpg"
+  },
+  {
+    "revision": "4e0e09997677d819612788c7261a7df6",
+    "url": "/ObjectOrientedProgramming.git/static/media/flechita.4e0e0999.png"
+  },
+  {
+    "revision": "542d357161b4b4d499a73ef4768b5350",
+    "url": "/ObjectOrientedProgramming.git/static/media/geometria.542d3571.jpg"
+  },
+  {
+    "revision": "2150b75a2cd95f745f044204203c7116",
+    "url": "/ObjectOrientedProgramming.git/static/media/logo.2150b75a.png"
+  },
+  {
+    "revision": "2f4895b2f8eade74d3b5a39419571d15",
+    "url": "/ObjectOrientedProgramming.git/static/media/marco.2f4895b2.png"
+  },
+  {
+    "revision": "710b277e6af1403c0009e14b08ef519f",
+    "url": "/ObjectOrientedProgramming.git/static/media/oscar.710b277e.jpg"
+  },
+  {
+    "revision": "a1caaeb26985645e47a12e22d8573d71",
+    "url": "/ObjectOrientedProgramming.git/static/media/papelesgeometria.a1caaeb2.png"
+  },
+  {
+    "revision": "8b3b03fcfa0bc0399c91be3b8791aa18",
+    "url": "/ObjectOrientedProgramming.git/static/media/quienes.8b3b03fc.png"
+  },
+  {
+    "revision": "d117821271d8e55c461569425412d793",
+    "url": "/ObjectOrientedProgramming.git/static/media/santiago.d1178212.jpg"
+  },
+  {
+    "revision": "5f35baf5f1567fa1b87b8439a97f3740",
+    "url": "/ObjectOrientedProgramming.git/static/media/yong.5f35baf5.jpg"
+  }
+]);
